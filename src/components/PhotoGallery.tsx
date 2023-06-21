249d3b4a8b475d7776cbd94f3edf9d23b21be2fd
Schnippen <dfojcik35@gmail.com>
2023-06-21 16:38:11 +0200

    modified:   src/components/PhotoGallery.tsx

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -12,7 +12,7 @@ const PhotoGallery = ({ photos, delay }: any) => {
       setTimeout(() => {
         setCurrentPhotoIndex((prevIndex) => (prevIndex + 1) % photos.length);
         setIsFadingOut(false);
-      }, 500); // Wait for the fade out transition to complete (adjust as needed)
+      }, 500);
     }, delay);
 
     return () => {
@@ -22,7 +22,7 @@ const PhotoGallery = ({ photos, delay }: any) => {
 
   const currentPhotoStyle = {
     opacity: isFadingOut ? 0 : 1,
-    transition: "opacity 0.5s ease-in-out", // Adjust the transition timing as needed
+    transition: "opacity 0.5s ease-in-out",
   };
 
   return (
