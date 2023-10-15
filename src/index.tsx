c0a6595af516fa2c2381ecc0a21de3be566e7e09
Schnippen <dfojcik35@gmail.com>
2023-10-15 14:14:54 +0200

    changed to hydration

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+const root = document.getElementById("root");
+
+ReactDOM.hydrate(
   <React.StrictMode>
     <BrowserRouter basename="/">
       <HelmetProvider>
         <App />
       </HelmetProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  root
 );
 
 // If you want to start measuring performance in your app, pass a function
